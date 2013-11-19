3044fdcc26a12701e021d6356781e7b5fa2ebc63
mvalerio <valerio.matt@gmail.com>
2013-11-19 18:05:36 -0500

    working on open order report parser

--- PDFtoCSV/src/com/thevalerios/pdf/PdfToCsv.java
+++ PDFtoCSV/src/com/thevalerios/pdf/PdfToCsv.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
 import com.itextpdf.text.io.RandomAccessSourceFactory;
 import com.itextpdf.text.pdf.PRTokeniser;
@@ -37,6 +38,8 @@ public class PdfToCsv {
 
 
 	public void parsePdf(String src, String dest) throws IOException {
+		boolean inSlsRep = false;
+		boolean inOrder = false;
         PdfReader reader = new PdfReader(src);
         // we can inspect the syntax of the imported page
         byte[] streamBytes = reader.getPageContent(1);
@@ -44,7 +47,58 @@ public class PdfToCsv {
         PrintWriter out = new PrintWriter(new FileOutputStream(dest,true));
         while (tokenizer.nextToken()) {
             if (tokenizer.getTokenType() == PRTokeniser.TokenType.STRING) {
-                out.println(tokenizer.getStringValue());
+            	String token = tokenizer.getStringValue();
+            	//System.out.println("token=" + token);
+            	if (token.startsWith("For Slsm:")) {
+            		inSlsRep = true;
+					System.out.println("new sales person");
+					String curSlsRep = token.substring(token.indexOf(":")+2,token.length());
+					System.out.println("curSlsRep="+ curSlsRep);
+				}
+            	else if (token.startsWith("Order#")){
+            		inOrder = true;
+            	}
+            	else if (token.startsWith("Slsm Totals:")){
+            		inSlsRep = false;
+            	}
+            	else if (token.startsWith("Order Taken By:")) {
+					inOrder=false;
+				}
+            	else if (token.startsWith("PO#")){
+            		
+            	}
+            	else{
+            		if (inSlsRep && inOrder) {	
+            			String[] items = token.split("\\s+");
+            			System.out.println(items.length);
+                		System.out.println("token=" + token);
+            			String ordDate = items[0];
+            			String ordPercent = items[1];
+            			String ordAmount = items[2];
+            			String ordCost = items[3];
+            			String ordGP = items[4];
+            			String ordShipDate = items[5];
+            			String ordCustName = new String();
+            			int i = 6;
+            			while (i<items.length) {
+							ordCustName.concat(items[i] + " ");
+							i++;
+						} 
+            			String custPO = items[items.length-1];
+            			System.out.println("ordDate = " + ordDate);
+            			System.out.println("ordPercent = " + ordPercent);
+            			System.out.println("ordAmount = " + ordAmount);
+            			System.out.println("ordCost = " + ordCost);
+            			System.out.println("ordGP = " + ordGP);
+            			System.out.println("ordShipDate = " + ordShipDate);
+            			System.out.println("ordCustName = " + ordCustName);
+            			System.out.println("custPO = " + custPO);
+                		
+					}
+            		
+            	}
+            	out.println(token);
+                
             }
         }
         out.flush();
